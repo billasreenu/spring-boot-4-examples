543764545f140bd2643d2bd1566df4ab3c1ac1cf
Sreenu BILLA <billasreenu123@gmail.com>
2025-08-31 14:54:02 +0530

    first commit

---
+++ src/main/java/com/billa/bfs/demo/Sbt4ExamplesApplication.java
@@ -0,0 +1,13 @@
+package com.billa.bfs.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Sbt4ExamplesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Sbt4ExamplesApplication.class, args);
+	}
+
+}
