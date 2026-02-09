240f0799f400d3ce52281dab041b24e9c9408bf6
Sreenu Billa <billasreenu4599@gmail.com>
2026-02-09 10:28:10 +0530

    update command line and read me line runners

--- spring-boot-4-application-runner/src/main/java/in/bsrtech/demo/runner/ApplicationRunnerExample.java
+++ spring-boot-4-application-runner/src/main/java/in/bsrtech/demo/runner/ApplicationRunnerExample.java
@@ -1,5 +1,8 @@
 package in.bsrtech.demo.runner;
 
+import java.util.List;
+import java.util.Set;
+
 import org.springframework.boot.ApplicationArguments;
 import org.springframework.boot.ApplicationRunner;
 import org.springframework.stereotype.Component;
@@ -10,6 +13,26 @@ public class ApplicationRunnerExample implements ApplicationRunner {
 	@Override
 	public void run(ApplicationArguments args) throws Exception {
 		IO.println("Spring Boot 4 application runner example");
+
+		IO.println("***** Print non option args");
+		List<String> nonOptionList = args.getNonOptionArgs();
+		for (String value : nonOptionList) {
+			System.out.println("-->" + value);
+		}
+
+		IO.println("***** Print option args");
+		Set<String> optionList = args.getOptionNames();
+		for (String value : optionList) {
+			System.out.println("-->" + value);
+		}
+
+		IO.println("***** Print un processed args");
+		String unProcessedArgs[] = args.getSourceArgs();
+		for (String value : unProcessedArgs) {
+			System.out.println("-->" + value);
+		}
+
+		IO.println("--------------------------------------");
 	}
 
 }
--- spring-boot-4-command-line-runner/src/main/java/in/bsrtech/demo/runner/CoomandLineRunnerExample.java
+++ spring-boot-4-command-line-runner/src/main/java/in/bsrtech/demo/runner/CoomandLineRunnerExample.java
@@ -10,6 +10,12 @@ public class CoomandLineRunnerExample implements CommandLineRunner {
 	public void run(String... args) throws Exception {
 		IO.println("Spring Boot 4 command line runner example");
 
+		int i = 1;
+
+		for (String arg : args) {
+			System.out.println(i + "-->" + arg);
+			i++;
+		}
 	}
 
 }
